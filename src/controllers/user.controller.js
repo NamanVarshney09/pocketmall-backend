ccbc8d189f368355355a091de7a7f42e88c9c8fd
Naman Varshney <namanvarshney09@gmail.com>
2024-09-04 07:05:17 +0530

    added register controller

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,62 @@
+import asyncHandler from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { User } from "../models/user.model.js";
+import uploadOnCloudinary from "../utils/cloudinary.js";
+
+const registerUser = asyncHandler(async (req, res) => {
+    const { fullName, email, username, password } = req.body;
+
+    if (
+        [fullName, email, username, password].some(
+            (field) => field?.trim() === ""
+        )
+    ) {
+        throw new ApiError(400, "All fields are required");
+    }
+    const existedUser = User.findOne({
+        $or: [{ username }, { email }],
+    });
+
+    if (existedUser) {
+        throw new ApiError(409, "User with email or username already exists");
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar not provided");
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar not uploaded");
+    }
+
+    const user = await User.create({
+        fullName,
+        avatar: avatar?.url || "",
+        email,
+        username: username.toLowerCase(),
+        password,
+    });
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    );
+
+    if (!createdUser) {
+        throw new ApiError(
+            500,
+            "Something went wrong while registering the user"
+        );
+    }
+
+    return res
+        .status(201)
+        .json(
+            new ApiResponse(200, createdUser, "User registered successfully")
+        );
+});
+
+export default registerUser;
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -28,6 +28,9 @@ const userSchema = new mongoose.Schema(
             type: String,
             required: [true, "Password is required"],
         },
+        avatar: {
+            type: String, // cloudinary url
+        },
         refreshToken: {
             type: String,
         },
