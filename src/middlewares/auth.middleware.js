a1788745e54c6aae95a11720e48bd099d332d950
Naman Varshney <namanvarshney09@gmail.com>
2024-09-30 01:20:58 +0530

    fixed major await bug

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -16,7 +16,7 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
 
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
-        const user = User.findById(decodedToken?._id).select(
+        const user = await User.findById(decodedToken?._id).select(
             "-password -refreshToken"
         );
         if (!user) {
